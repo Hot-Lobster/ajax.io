3913649ff834cdbd243306036653f3277f671ed5
olsoa4 <olsoa4@hotmail.com>
2018-08-18 00:14:45 -0500

    maybe

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 	function displaycartoonInfo(){     
 		$('#cartoonsView').empty();     
 		var cartoon = $(this).attr('data-name');
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + cartoon + "&api_key=dc6zaTOxFJmzC&limit=10";
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + cartoon + "&api_key=dc6zaTOxFJmzC&limit=10";
 		$.ajax({url: queryURL, method: 'GET'})
 	     .done(function(response) {
 	         var results = response.data;
